d3eba782de0226ebb1cb232cd2543629a103796e
sober <gongyi964@gmail.com>
2026-07-11 17:15:13 +0800

    refactor(（package/core）): :recycle: redesign useToggle hook to support custom reverse value

--- packages/core/src/use-toggle/index.ts
+++ packages/core/src/use-toggle/index.ts
@@ -1,25 +1,37 @@
-import { isBoolean, isUndefined } from '@sober/react-use-shared';
+import { isBoolean } from '@sober/react-use-shared';
 import { useCallback, useState } from 'react';
 
-function useToggle<T = boolean>(
-  defaultValue: T = false as unknown as T
-): [T, (nextValue?: T | boolean) => void] {
-  const [state, setState] = useState<T>(defaultValue);
+/**
+ * 切换状态的hooks
+ * @param defaultValue 默认值
+ * @param reverseValue 反转值
+ */
+function useToggle<T, R = T>(defaultValue: T, reverseValue: R): [T | R, () => void];
+/**
+ * 切换状态的hooks
+ * @param defaultValue 默认值 ，默认false
+ */
+function useToggle<T = boolean>(defaultValue?: T): [T | boolean, () => void];
 
-  const toggle = useCallback(
-    (nextValue?: T | boolean) => {
-      if (isBoolean(nextValue)) {
-        setState(nextValue as T);
-      } else if (!isUndefined(nextValue)) {
-        setState(nextValue);
-      } else {
-        setState((prev) => {
-          return (isBoolean(prev) ? !prev : defaultValue) as T;
-        });
-      }
-    },
-    [defaultValue]
-  );
+function useToggle<T, R = T>(...args: [defaultValue?: T | boolean, reverseValue?: R]) {
+  const defaultValue = args.length >= 1 ? args[0] : false;
+  const reverseValue = args.length >= 2 ? args[1] : defaultValue;
+  const hasReverseValue = args.length >= 2;
+
+  const actualReverseValue = hasReverseValue
+    ? reverseValue
+    : isBoolean(defaultValue)
+      ? !defaultValue
+      : defaultValue;
+
+  const [state, setState] = useState<T | R | boolean | undefined>(defaultValue);
+
+  const toggle = useCallback(() => {
+    setState((prev) => {
+      return prev === defaultValue ? actualReverseValue : defaultValue;
+    });
+  }, [defaultValue, actualReverseValue]);
+
   return [state, toggle];
 }
 
